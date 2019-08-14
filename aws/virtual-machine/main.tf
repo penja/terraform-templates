b04ec57c303a83231cc9c98ab9227c88405821e8
penja <a.penchuk@scalr.com>
2019-08-14 13:02:47 +0300

    change add new instance

--- aws/virtual-machine/main.tf
+++ aws/virtual-machine/main.tf
@@ -18,6 +18,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "terraform-test-instance" {
+  count = 2
   ami             = "${data.aws_ami.ubuntu.id}"
   instance_type   = "${var.instance_type}"
 
