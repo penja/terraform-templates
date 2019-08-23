97007cdf9a047f3e6e5a94e9412add189c271edc
Andrii Penchuk <a.penchuk@scalr.com>
2019-08-23 18:46:57 +0300

    Update main.tf

--- aws/virtual-machine/main.tf
+++ aws/virtual-machine/main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"]
 }
 
-resource "aws_instance" "terraform-test-instance-ape2" {
+resource "aws_instance" "terraform-test-instance-ape22" {
   ami             = "${data.aws_ami.ubuntu.id}"
   instance_type   = "t2.nano"
 
